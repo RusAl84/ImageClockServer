cff7a4d925ffaf5308f56611239579bded8354f5
Rusal84 <rusal@bk.ru>
2021-05-05 04:41:14 +0300

    add fixes

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 import os
-
+from stylize import render
 from PIL import Image
 from flask import Flask,flash, request, redirect, url_for
 from werkzeug.utils import secure_filename
+import cv2
+# from scipy.misc import imread, imsave
+# # pip install scipy==1.1.0
+import imageio
+
+
 
 UPLOAD_FOLDER = './upload'
 
@@ -28,11 +34,25 @@ def SendMessage():
         if file:
             filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        image = Image.open(UPLOAD_FOLDER + '/image.jpg')
+        # image = Image.open(UPLOAD_FOLDER + '/image.jpg').convert('RGB')
+        image = cv2.imread(UPLOAD_FOLDER + '/image.jpg')
+        #file.save(os.path.join('./static/image.jpg'))
+        #abstract = render(image, depth=4, verbose=True)
+        smoother = render(image, iterations=25, verbose=True)
+        # aa = render(image, anti_aliasing=True, verbose=True)
+        #less_detail = render(image, ratio=0.001, verbose=True)
+        # more_detail = render(image, ratio=0.00005, verbose=True)
+        #landmarks = render(image, features='landmarks', verbose=True)
+        # defaults = render(image, verbose=True)
+        imageio.imwrite('./static/image.jpg', smoother)
+        # imsave('./static/image.jpg', landmarks)
+        # abstract = abstract.save('./static/image.jpg')
+        # imsave(r'./static/image.jpg', abstract)
+        # show_img(abstract, "A depth of 4 results in an abstract representation")
+        image = Image.open('./static/image.jpg')
         image.show()
-        return redirect(url_for('read_uploaded_file',
-                                filename=filename)) , 200
-    return "" , 500
+        return {"id":1,"Username":"admin","Level":"Administrator"} , 200
+    return "http://10.0.2.2:5000/upload/image.jpg" , 200
 
 
 
